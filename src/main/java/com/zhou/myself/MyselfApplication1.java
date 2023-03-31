3e9a7cce8a26faa8037128018a6fdeb8ca61e189
zhouzhou2023 <13529744829@163.com>
2023-03-31 22:49:27 +0800

    22.40

---
+++ src/main/java/com/zhou/myself/MyselfApplication1.java
@@ -0,0 +1,13 @@
+package com.zhou.myself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyselfApplication1 {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyselfApplication1.class, args);
+	}
+
+}
