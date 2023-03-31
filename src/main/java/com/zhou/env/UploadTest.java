9262c7344bb0398334219c9c6383d1cb0d655992
zhouzhou2023 <13529744829@163.com>
2023-03-31 22:21:56 +0800

    修改666为777

--- src/main/java/com/zhou/env/UploadTest.java
+++ src/main/java/com/zhou/env/UploadTest.java
@@ -5,7 +5,7 @@ package com.zhou.env;
  */
 public class UploadTest {
     public static void main(String[] args) {
-        System.out.println(666666666);
+        System.out.println(77777777);
     }
 
 }
